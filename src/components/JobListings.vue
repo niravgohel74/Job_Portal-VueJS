6e3b596c67158392510374a714b80488fce3ac94
niravgohel74 <niravgohel2000@gmail.com>
2024-09-22 16:03:44 +0530

    changes

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -51,7 +51,7 @@ const state = reactive({
 
 onMounted(async () => {
   try {
-    const response = await axios.get('/api/jobs');
+    const response = await axios.get('/data/jobs.json'); // Update this line
     state.jobs = response.data;
   } catch (error) {
     console.error('Error Fetching Jobs', error);
